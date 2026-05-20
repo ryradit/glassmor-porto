04d797958a62cc284a13d113cc9540e43c3e3851
Ryan Radityatama <ryanradityatama@Ryans-MacBook-Air-2.local>
2026-05-20 18:03:58 +0700

    chore: remove scratch file that was causing build errors

--- src/lib/scratch.ts
+++
@@ -1,5 +0,0 @@
-import { Document, Packer, Paragraph, TextRun, HeadingLevel, BorderStyle, Table, TableRow, TableCell, WidthType, AlignmentType, ShadingType, convertInchesToTwip } from 'docx';
-import { saveAs } from 'file-saver';
-import { CVData } from './cv-builder';
-
-// Keep all the logic here
